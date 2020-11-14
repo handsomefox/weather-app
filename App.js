8a7c2b8ff1d203a4a699314ff53ad804bb4f89b2
handsomefox <handsomefox.hf@gmail.com>
2020-11-14 16:06:54 +0200

    initialize repo

---
+++ App.js
@@ -0,0 +1,27 @@
+var city = document.getElementById('location-city');
+city.addEventListener("input", UpdateWeather);
+function KelvinToCelcius(K) {
+    return K - 273.15;
+}
+
+async function UpdateWeather(event) {
+    if (event.type == 'input') {
+        api = `http://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${config.apiKey}`;
+        fetch(api).then(response => {
+            return response.json();
+        }).then(WeatherData => {
+            console.log(WeatherData);
+            const { } = WeatherData.main;
+            temperature = document.getElementById('temp')
+
+            icon = `http://openweathermap.org/img/wn/${WeatherData.weather[0].icon}@2x.png`;
+
+
+            document.getElementById('temp').innerHTML = KelvinToCelcius(WeatherData.main.temp).toFixed(2) + " C°";
+            document.getElementById('icon').innerHTML = `<img src="${icon}">`;
+        });
+    }
+    else {
+        console.log("Couldn't read weather input.")
+    }
+}
