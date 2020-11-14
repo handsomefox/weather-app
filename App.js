566deaed33f54ff28379f64ea8a543d20b5bd9e2
handsomefox <handsomefox.hf@gmail.com>
2020-11-14 23:44:57 +0200

    Reformat App.js, remove console.log

--- App.js
+++ App.js
@@ -1,27 +1,31 @@
-var city = document.getElementById('location-city');
+var city = document.getElementById("location-city");
 city.addEventListener("input", UpdateWeather);
-function KelvinToCelcius(K) {
-    return K - 273.15;
+
+function KelvinToCelcius(K) 
+{
+  return K - 273.15;
 }
 
-async function UpdateWeather(event) {
-    if (event.type == 'input') {
-        api = `http://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${config.apiKey}`;
-        fetch(api).then(response => {
-            return response.json();
-        }).then(WeatherData => {
-            console.log(WeatherData);
-            const { } = WeatherData.main;
-            temperature = document.getElementById('temp')
+async function UpdateWeather(event) 
+{
+  if (event.type == "input") 
+  {
+    api = `http://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${config.apiKey}`;
+    fetch(api)
+      .then((response) => 
+      {
+        return response.json();
+      })
+      .then((WeatherData) => 
+      {
+        const {} = WeatherData.main;
 
-            icon = `http://openweathermap.org/img/wn/${WeatherData.weather[0].icon}@2x.png`;
+        temperature = document.getElementById("temp");
 
+        icon = `http://openweathermap.org/img/wn/${WeatherData.weather[0].icon}@2x.png`;
 
-            document.getElementById('temp').innerHTML = KelvinToCelcius(WeatherData.main.temp).toFixed(2) + " C°";
-            document.getElementById('icon').innerHTML = `<img src="${icon}">`;
-        });
-    }
-    else {
-        console.log("Couldn't read weather input.")
-    }
+        document.getElementById("temp").innerHTML = KelvinToCelcius(WeatherData.main.temp).toFixed(2) + " C°";
+        document.getElementById("icon").innerHTML = `<img src="${icon}">`;
+      });
+  }
 }
